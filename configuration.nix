6df54f0014412cc541a29a8fb732ebae14bac8da
Dale Morris <dalemorris2021@gmail.com>
2026-07-10 22:24:33 -0400

    Initial commit

---
+++ configuration.nix
@@ -0,0 +1,232 @@
+# Edit this configuration file to define what should be installed on
+# your system.  Help is available in the configuration.nix(5) man page
+# and in the NixOS manual (accessible by running ‘nixos-help’).
+
+{ config, pkgs, lib, ... }:
+
+let
+  home-manager = builtins.fetchTarball https://github.com/nix-community/home-manager/archive/release-26.05.tar.gz;
+in
+{
+  imports =
+    [ # Include the results of the hardware scan.
+      ./hardware-configuration.nix
+      (import "${home-manager}/nixos")
+    ];
+
+  # Bootloader.
+  boot.loader.systemd-boot.enable = true;
+  boot.loader.efi.canTouchEfiVariables = true;
+
+  # Use latest kernel.
+  boot.kernelPackages = pkgs.linuxPackages_latest;
+
+  networking.hostName = "owl"; # Define your hostname.
+  # networking.wireless.enable = true;  # Enables wireless support via wpa_supplicant.
+
+  # Configure network proxy if necessary
+  # networking.proxy.default = "http://user:password@proxy:port/";
+  # networking.proxy.noProxy = "127.0.0.1,localhost,internal.domain";
+
+  # Enable networking
+  networking.networkmanager.enable = true;
+
+  # Set your time zone.
+  time.timeZone = "America/Indiana/Indianapolis";
+
+  # Select internationalisation properties.
+  i18n.defaultLocale = "en_US.UTF-8";
+
+  i18n.extraLocaleSettings = {
+    LC_ADDRESS = "en_US.UTF-8";
+    LC_IDENTIFICATION = "en_US.UTF-8";
+    LC_MEASUREMENT = "en_US.UTF-8";
+    LC_MONETARY = "en_US.UTF-8";
+    LC_NAME = "en_US.UTF-8";
+    LC_NUMERIC = "en_US.UTF-8";
+    LC_PAPER = "en_US.UTF-8";
+    LC_TELEPHONE = "en_US.UTF-8";
+    LC_TIME = "en_US.UTF-8";
+  };
+
+  # Enable the X11 windowing system.
+  # You can disable this if you're only using the Wayland session.
+  services.xserver.enable = true;
+
+  # Enable the KDE Plasma Desktop Environment.
+  services.displayManager.sddm.enable = true;
+  services.desktopManager.plasma6.enable = true;
+
+  # Configure keymap in X11
+  services.xserver.xkb = {
+    layout = "us";
+    variant = "";
+  };
+
+  # Enable CUPS to print documents.
+  services.printing.enable = true;
+
+  # Enable sound with pipewire.
+  services.pulseaudio.enable = false;
+  security.rtkit.enable = true;
+  services.pipewire = {
+    enable = true;
+    alsa.enable = true;
+    alsa.support32Bit = true;
+    pulse.enable = true;
+    # If you want to use JACK applications, uncomment this
+    #jack.enable = true;
+
+    # use the example session manager (no others are packaged yet so this is enabled by default,
+    # no need to redefine it in your config for now)
+    #media-session.enable = true;
+  };
+
+  # Enable touchpad support (enabled default in most desktopManager).
+  # services.xserver.libinput.enable = true;
+
+  # Define a user account. Don't forget to set a password with ‘passwd’.
+  users.users."ethereal" = {
+    isNormalUser = true;
+    description = "Ethereal";
+    extraGroups = [ "networkmanager" "wheel" ];
+    packages = with pkgs; [
+      # Simple packages for this user
+    ];
+
+    # Packages with config for this user
+
+    shell = pkgs.zsh;
+    useDefaultShell = false;
+  };
+  
+  home-manager.users."ethereal" = {pkgs, ...}: {
+    home.packages = with pkgs; [
+      # Simple packages for this user (home manager)
+      kdePackages.kate
+      fira-code
+    ];
+
+    # Packages with config for this user (home manager)
+    programs.wezterm = {
+      enable = true;
+      enableZshIntegration = true;
+      extraConfig = ''
+        local wezterm = require('wezterm')
+
+	local config = {}
+
+	config.color_scheme = 'Tokyo Night'
+	config.font = wezterm.font('Fira Code')
+	config.window_background_opacity = 0.5
+
+        config.keys = {
+          {
+	    key = "'",
+	    mods = 'CTRL',
+	    action = wezterm.action.SplitHorizontal { domain = 'CurrentPaneDomain' },
+	  },
+          {
+	    key = '"',
+	    mods = 'CTRL|SHIFT',
+	    action = wezterm.action.SplitVertical { domain = 'CurrentPaneDomain' },
+	  },
+	  {
+	    key = 'LeftArrow',
+	    mods = 'CTRL',
+	    action = wezterm.action.ActivatePaneDirection 'Left',
+	  },
+	  {
+	    key = 'RightArrow',
+	    mods = 'CTRL',
+	    action = wezterm.action.ActivatePaneDirection 'Right',
+	  },
+	  {
+	    key = 'UpArrow',
+	    mods = 'CTRL',
+	    action = wezterm.action.ActivatePaneDirection 'Up',
+	  },
+	  {
+	    key = 'DownArrow',
+	    mods = 'CTRL',
+	    action = wezterm.action.ActivatePaneDirection 'Down',
+	  },
+	  {
+	    key = 'w',
+	    mods = 'CTRL',
+	    action = wezterm.action.CloseCurrentPane { confirm = true },
+	  },
+	}
+
+	return config
+      '';
+    };
+
+    programs.neovim = {
+      enable = true;
+      defaultEditor = true;
+    };
+
+    programs.git = {
+      enable = true;
+      settings = {
+        user = {
+	  name = "Dale Morris";
+          email = "dalemorris2021@gmail.com";
+	};
+        init.defaultBranch = "main";
+      };
+    };
+
+    # This value determines the Home Manager release that your configuration is
+    # compatible with. This helps avoid breakage when a new Home Manager release
+    # introduces backwards incompatible changes.
+    #
+    # You should not change this value, even if you update Home Manager. If you do
+    # want to update the value, then make sure to first check the Home Manager
+    # release notes.
+    home.stateVersion = "26.05"; # Please read the comment before changing.
+  };
+
+  # System wide packages with config
+  programs.firefox.enable = true;
+  programs.zsh.enable = true;
+
+  # Allow unfree packages
+  nixpkgs.config.allowUnfree = true;
+
+  # List packages installed in system profile. To search, run:
+  # $ nix search wget
+  environment.systemPackages = with pkgs; [
+    vim
+    wget
+  ];
+
+  # Some programs need SUID wrappers, can be configured further or are
+  # started in user sessions.
+  # programs.mtr.enable = true;
+  # programs.gnupg.agent = {
+  #   enable = true;
+  #   enableSSHSupport = true;
+  # };
+
+  # List services that you want to enable:
+
+  # Enable the OpenSSH daemon.
+  # services.openssh.enable = true;
+
+  # Open ports in the firewall.
+  # networking.firewall.allowedTCPPorts = [ ... ];
+  # networking.firewall.allowedUDPPorts = [ ... ];
+  # Or disable the firewall altogether.
+  # networking.firewall.enable = false;
+
+  # This value determines the NixOS release from which the default
+  # settings for stateful data, like file locations and database versions
+  # on your system were taken. It‘s perfectly fine and recommended to leave
+  # this value at the release version of the first install of this system.
+  # Before changing this value read the documentation for this option
+  # (e.g. man configuration.nix or on https://nixos.org/nixos/options.html).
+  system.stateVersion = "26.05"; # Did you read the comment?
+
+}
